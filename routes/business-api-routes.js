3e9233d9e0f362f49d18e57501454e1f4dace380
Chris Rico <chris.r.rico@gmail.com>
2020-06-08 15:02:37 -0700

    added seeds file

--- routes/business-api-routes.js
+++ routes/business-api-routes.js
@@ -5,32 +5,32 @@ module.exports = function(app) {
   // GET route for getting all of the posts
   app.get("/api/business", function(req, res) {
     var query = {};
-    if (req.query.location_id) {
-      query.AuthorId = req.query.author_id;
+    if (req.query.name) {
+      query.name = req.query.name;
     }
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
-    db.Post.findAll({
+    db.Business.findAll({
       where: query,
-      include: [db.Business]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+      include: [db.User]
+    }).then(function(dbBusiness) {
+      res.json(dbBusiness);
     });
   });
 
   // Get route for retrieving a single post
-  app.get("/api/business/:id", function(req, res) {
+  app.get("/api/business/:type", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
-    db.Post.findOne({
+    db.Business.findAll({
       where: {
-        id: req.params.id
-      },
-      include: [db.Business]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+        type: req.params.type
+      }
+    }).then(function(result) {
+      res.json(result);
+      console.log(result);
     });
   });
 
@@ -41,3 +41,5 @@ module.exports = function(app) {
     });
   });
 };
+
+// 
