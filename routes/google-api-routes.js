e83e3136c496dd211e9c3aadc448390fa02d9974
Chris Rico <chris.r.rico@gmail.com>
2020-06-08 10:00:41 -0700

    ok

--- routes/google-api-routes.js
+++ routes/google-api-routes.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 module.exports = (app)=>{
     try {
+        
         app.post('/api/google/search', async(req,res)=>{
-        const result = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/
-        json?key=AIzaSyBCikoA8ksw5bkOF7CpOMW26-iN3dOwPLE&location=34.0157309,
-        -118.5015921&radius=5000&keyword=bar`)
-       res.json(result)
+            console.log('searching....')
+        const {data} = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBCikoA8ksw5bkOF7CpOMW26-iN3dOwPLE&location=34.0157309,-118.5015921&radius=5000&keyword=bar`)
+       res.json(data.results)
     }) 
     }catch(err){
         console.log(err)
