86b9f1a4375776845f183c8b217c6e6112ac905c
AlkaTone <antonio.a.garces@gmail.com>
2020-06-09 13:13:22 -0700

    changed some seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,7 +9,7 @@ INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Playhous
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Lure Nightclub", "nightclub", "1439 Ivar Ave, Los Angeles, CA 90028", 34.0971, -118.3290, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Bar Sinister", "nightclub", "1652 N Cherokee Ave, Los Angeles, CA 90028", 34.1011, -118.3349, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Lash", "nightclub", "117 Winston St, Los Angeles, CA 90013", 34.0472, -118.2472, 1);
-INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Crocker Club", "nightclub", "453 S Spring St, Los Angeles, CA 90013", 34.047661, -118.251823, 1);
+INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Wolf & Crane", "nightclub", "366 E 2nd St, Los Angeles, CA 90012", 34.047849, -118.239764, 1);
 
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Vibrato Grill Jazz", "jazz", "2930 Beverly Glen Cir, Los Angeles, CA 90077", 34.127257, -118.443605, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("BlueWhale", "jazz", "123 Astronaut Ellison S Onizuka St #301, Los Angeles, CA 90012", 34.0499, -118.2421, 1);
@@ -20,23 +20,23 @@ INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Bar Tosc
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Venice Beach Bar", "cafe", "323 Ocean Front Walk, Venice, CA 90291", 33.9941151, -118.4798821, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Perch", "cafe", "448 S Hill St, Los Angeles, CA 90013", 34.0490, -118.2514, 1);
 
-INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Misfit Restaurant + Bar", "lounge", "225 Santa Monica Blvd, Santa Monica, CA 90401", 34.0154445, -118.4966717, 1);
+INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Venice Room", "lounge", "2428 S Garfield Ave, Monterey Park, CA 91754", 34.034150, -118.129978, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("EL DORADO", "lounge", "416 S Spring St, Los Angeles, CA 90013", 34.048382, -118.248493, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Rhythm Room L.A.", "lounge", "206 W 6th St BSMT, Los Angeles, CA 90014", 33.4944, -112.0597, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Rooftop", "lounge", "550 Flower St, Los Angeles, CA 90071", 34.0502, -118.2571, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Little Easy", "lounge", "215 W 5th St, Los Angeles, CA 90013", 34.0476,-118.2502, 1);
-INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Daily Pint", "lounge", "2310 Pico Blvd, Santa Monica, CA 90405", 34.021169, -118.466180, 1);
+INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Spirit House", "lounge", "123 S Lincoln Ave, Monterey Park, CA 91755", 34.061822, -118.121623, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Black Cat", "lounge", "3909 Sunset Blvd, Los Angeles, CA 90029", 34.092060, -118.279756, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("4100 Bar", "lounge", "1087 Manzanita St, Los Angeles, CA 90029", 34.093024, -118.281494, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Jay's Bar", "lounge", "4321 Sunset Blvd, Los Angeles, CA 90029", 34.095319, -118.283115, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Library Bar", "lounge", "7000 Hollywood Blvd, Los Angeles, CA 90028", 34.101257, -118.341609, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Power House Bar", "lounge", "1714 N Highland Ave, Los Angeles, CA 90028", 34.102137, -118.338496, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The North End", "lounge", "2626 Hermosa Ave, Hermosa Beach, CA 90254", 33.8725, -118.4052, 1);
-INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Rick's Tavern On Main", "lounge", "2907 Main St, Santa Monica, CA 90405", 33.999443, -118.480689, 1);
+INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Belly's West", "lounge", "1156 W Valley Blvd, Alhambra, CA 91803", 34.077599, -118.135772, 1);
 
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Gamez DTLA", "sports", "500 S Spring St, Los Angeles, CA 90013", 34.047225 ,-118.249588, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("El Tejano", "sports", "801 S Grand Ave, Los Angeles, CA 90017", 34.0453, -118.258918, 1);
-INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Buffalo Wild Wings", "sports", "3150 Wilshire Blvd Suite #100, Los Angeles, CA 90010", 34.061256, -118.290988, 1);
+INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Buffalo Wild Wings", "sports", "4000 Market Pl Dr Suite 104, Monterey Park, CA 91755", 34.036728, -118.099690, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Busby's East", "sports", "5364 Wilshire Blvd, Los Angeles, CA 90036", 34.062069, -118.345844, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("Rocco's Tavern", "sports", "1000 Gayley Ave, Los Angeles, CA 90024", 34.061943, -118.447759, 1);
 INSERT INTO Businesses (name, type, address, lat, lng, UserId) VALUES ("The Garage on Motor Ave", "sports", "3371 Motor Ave, Los Angeles, CA 90034", 34.027673, -118.409603, 1);
